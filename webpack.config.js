4ea7d569fa7e15fa74bbb7a50a8f827b91eb80fa
Aung Myo Kyaw <amk.maeu@gmail.com>
2017-07-24 19:19:06 +0630

    build using webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+	entry:{
+		zuitch:path.resolve(__dirname,'src/zuitch.js')
+	},
+	output:{
+		path:path.resolve(__dirname,'docs/dist'),
+		filename:'[name].js'
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js$/,
+				exclude: /node_modules\/(?!knayi-myscript)/,
+				use:{
+					loader:'babel-loader',
+					options:{
+						presets:['env']
+					}
+				}
+			}
+		]
+	},
+	plugins:[
+		new webpack.optimize.UglifyJsPlugin()
+	],
+	target:'web'
+}
+
+module.exports = config;
